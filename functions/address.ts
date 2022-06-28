b0b9d93de380c1248dbd882134fe578f4e17b859
bridgerz <bridgerzoske@gmail.com>
2022-06-28 15:23:37 -0700

    Update address.ts

--- functions/address.ts
+++ functions/address.ts
@@ -12,8 +12,8 @@ export const trimAddress = (address: string, symbols: number): string => {
   if (symbols <= 0) return ""
   if (symbols * 2 >= address.length) return address
 
-  const left = address.slice(0, symbols)
-  const right = address.slice(-symbols)
+  let left = address.slice(0, symbols)
+  let right = address.slice(-symbols)
 
   return `${left}...${right}`
 }
